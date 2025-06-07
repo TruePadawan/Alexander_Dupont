49fc4149adde2a08b64d054b221285e1f5756001
hermes <hermeschigoziri@gmail.com>
2025-06-07 19:32:34 +0100

    implement fake lang switcher

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const openMenuButton = document.getElementById("hamburger-menu-btn");
 const closeMenuButton = document.getElementById("close-menu-btn");
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileNavEls = mobileMenu.querySelectorAll("nav a");
+const engSwitch = document.querySelectorAll(".eng-switch");
+const frSwitch = document.querySelectorAll(".fr-switch");
 
 function closeMobileMenu() {
     const menuIsClosed = openMenuButton.classList.toggle("menu-open") === false;
@@ -17,9 +19,25 @@ function openMobileMenu() {
     }
 }
 
+function switchLanguage() {
+    engSwitch.forEach(el => {
+        el.classList.toggle("text-orange-400");
+    })
+    frSwitch.forEach(el => {
+        el.classList.toggle("text-orange-400");
+    });
+}
+
 openMenuButton.onclick = openMobileMenu
 closeMenuButton.onclick = closeMobileMenu
 
 mobileNavEls.forEach(anchorEl => {
     anchorEl.addEventListener("click", closeMobileMenu)
+})
+
+engSwitch.forEach(el => {
+    el.addEventListener("click", switchLanguage);
+})
+frSwitch.forEach(el => {
+    el.addEventListener("click", switchLanguage);
 })
